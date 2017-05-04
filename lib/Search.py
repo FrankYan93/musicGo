fe72db2a20400a9dab1a3eba25ae98c98867d99f
xyjiang94 <xyjiang94@gmail.com>
2017-05-04 16:28:42 -0400

    build query

--- lib/Search.py
+++ lib/Search.py
@@ -1,70 +1,68 @@
 import json
 import re
-from lib.Track import *
+from Track import *
 
-def build_qurey(description, artist, genre, album):
+def build_qurey(s, d_query):
     # build description query if exists
-    if (len(description) > 0):
+    if 'description' in d_query:
         q = Q("bool",
             must=[
-            Q("multi_match", query=description, fields=['title', 'lyric'], operator='and'),
+            Q("multi_match", query = d_query['description'], fields=['title', 'lyric', 'artist_name', 'artist_location', 'genres', 'album'], operator='and'),
             ],
         )
         s = s.query(q)
 
     # build query according to artists if exists
-    if (len(artist) > 0):
-        print 42
-        queries = []
-        for name in process_star(starring):
-            queries += [Q('match', artist=name)]
-
-        s = s.query(Q('bool', **{
-                'should': queries
-            }))
+    if 'artist_name' in d_query:
+        s = s.filter(Q('match', artist_name = d_query['artist_name']))
+        # s = s.sort('artist_name')
 
 
     #build genre filter if exists
-    if (genre != 'None'):
+    if 'genre' in d_query:
         print 52
-        s = s.filter(Q('match', genre=genre))
+        s = s.filter(Q('match', genres = d_query['genre']))
 
 
     # set hightlight in title and text
-    s = s.highlight_options(order='score')
-    s = s.highlight("lyric")
-    s = s.highlight('title')
+    # s = s.highlight_options(order='score')
+    # s = s.highlight("lyric")
+    # s = s.highlight('title')
 
     print(s.to_dict())
     return s
 
-
-def search(title, artist, genre, album, lyric):
+# Parameters
+# ----------
+# d_query: dictionary
+#     a dictionary that stores every field of the query
+#     value string : description, artist_name, artist_location, album
+#     value range: duration_l, duration_h, year_l, year_h
+#     value list of string: genre
+# Returns
+# -------
+# a list of dictionary
+def search(d_query):
     connections.create_connection(hosts=['localhost'])
     Track.init()
 
     s = Track.search()
-    s = build_qurey(title, artist, genre, album, lyric)
+    s = build_qurey(s,d_query)
     results = s.execute()
 
     l_results = []
     s = s[0:results.hits.total]
-    for doc in s:
-        dict_movie = {}
-        dict_movie['id'] = doc.meta.id
-        dict_movie['score'] = doc.meta.score
-        try:
-            dict_movie['title'] = process_highlight(doc.meta.highlight.title, doc.title)
-        except:
-            dict_movie['title'] = doc.title
-        try:
-            dict_movie['lyric'] = process_highlight(doc.meta.highlight.lyric, doc.text)[0:200]
-        except:
-            dict_movie['lyric'] = doc.text[0:200]
+    for track in s:
+        dict_track = {}
+        dict_track['id'] = track.meta.id
+        dict_track['score'] = track.meta.score
+        dict_track['title'] = track.title
+        dict_track['lyric'] = track.lyric[0:200]
 
 
-        l_results.append(dict_movie)
+        l_results.append(dict_track)
     return l_results
 
-# d = search('la la land', '', 'None', '', '' )
+d_query = {'description':'love','artist_name':'john'}
+print search(d_query)
 # print d
